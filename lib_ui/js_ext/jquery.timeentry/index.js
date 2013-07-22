f8d336c3f83601ce1eb87f3e6555b0f5310bbd44
Y.Toyosawa <toyosawa@sharingseed.co.jp>
2013-07-22 17:00:22 +0900

    -

--- lib_ui/js_ext/jquery.timeentry/index.js
+++
@@ -1,5 +0,0 @@
-
-	rui.require_js("jquery.timeentry/jquery.timeentry.min.js");
-	rui.require_js("jquery.timeentry/jquery.timeentry-ja.js");
-	rui.require_css("jquery.timeentry/jquery.timeentry.css");
-	
