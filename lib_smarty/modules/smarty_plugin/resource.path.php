82d1642e280eed7e463a7cd538447066e0e41f2f
Y.Toyosawa <toyosawa@sharingseed.co.jp>
2014-11-15 19:03:19 +0900

    -

--- lib_smarty/modules/smarty_plugin/resource.path.php
+++ lib_smarty/modules/smarty_plugin/resource.path.php
@@ -1,13 +1,13 @@
 <?php
 
-	function smarty_resource_path_source ($tpl_name, $tpl_source, $smarty) {
+	function smarty_resource_path_source ($tpl_name, &$tpl_source, $smarty) {
 		
 		$file =$smarty->resolve_resource_path($tpl_name,true);
 		$tpl_source =file_get_contents($file);
 		return true;
 	}
 	
-	function smarty_resource_path_timestamp($tpl_name, $tpl_timestamp, $smarty) {
+	function smarty_resource_path_timestamp($tpl_name, &$tpl_timestamp, $smarty) {
 	
 		$file =$smarty->resolve_resource_path($tpl_name);
 		$tpl_timestamp =filemtime($file);
