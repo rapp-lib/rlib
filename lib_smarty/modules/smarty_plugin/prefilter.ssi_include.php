0d3d423b20c25f28461807617363833fce9b41c2
Noname <noname@invalance.com>
2015-12-31 16:10:13 +0900

    -

--- lib_smarty/modules/smarty_plugin/prefilter.ssi_include.php
+++ lib_smarty/modules/smarty_plugin/prefilter.ssi_include.php
@@ -2,9 +2,10 @@
 
     function smarty_prefilter_ssi_include ($src, &$smarty){
         
+        // ssi virtual=ではなく、include file=として展開する
         $src =preg_replace_callback('!<\!--#include\s+virtual="([^"]+)"\s*-->!',function ($match) use ($smarty){
             $file =registry("Path.document_root_dir")."/".$match[1];
-            return $smarty->left_delimiter.'ssi virtual="'.$file.'"'.$smarty->right_delimiter;
+            return $smarty->left_delimiter.'include file="'.$file.'"'.$smarty->right_delimiter;
         },$src);
         
         $src =preg_replace_callback('!<\!--#include\s+file="([^"]+)"\s*-->!',function ($match) use ($smarty){
