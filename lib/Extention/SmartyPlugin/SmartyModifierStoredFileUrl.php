532a321f198ae501b883cc12dfc6a6dc70c14343
DevelopmentTeam <dev@localhost>
2017-03-03 17:41:14 +0900

    -

--- lib/Extention/SmartyPlugin/SmartyModifierStoredFileUrl.php
+++ lib/Extention/SmartyPlugin/SmartyModifierStoredFileUrl.php
@@ -11,6 +11,6 @@ class SmartyModifierStoredFileUrl
      */
     function callback ($code, $default=null)
     {
-        return $code ? webroot()->getAttr("webroot_url")."/file:/".$code : $default;
+        return $code ? app()->router->getWebroot()->getConfig("webroot_url")."/file:/".$code : $default;
     }
 }
