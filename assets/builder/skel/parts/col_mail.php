c8abac737b38635f8051b152bee42342308e3de2
DevelopmentTeam <dev@localhost>
2017-08-24 05:31:33 +0900

    -

--- assets/builder/skel/parts/col_mail.php
+++ assets/builder/skel/parts/col_mail.php
@@ -3,15 +3,15 @@
     $name = $col->getName();
 ?>
 <? if ($col->getAttr("type")=="assoc"): ?>
-<?="\n"?>
-<#?php foreach <?=$var_name?>["<?=$name?>"] as $key=>$item): ?>
+--<?="\n"?>
+<#?php foreach (<?=$var_name?>["<?=$name?>"] as $key=>$item): ?>
 <?php foreach ($col->getAssocTable()->getInputCols() as $assoc_col): ?>
   <?=$assoc_col->getLabel()?> : <?=$assoc_col->getMailSource(array("var_name"=>'$item'))?><?="\n"?>
 <?php endforeach; /* foreach as $assoc_col */ ?>
 --<?="\n"?>
 <#?php endforeach; ?><?=""?>
 <?php elseif ($enum_set = $col->getEnumSet()): /* if type=="assoc" */ ?>
-<#?=app()->enum("<?=$enum_set->getFullName()?>")->offsetGet(<?=$var_name?>["<?=$name?>"])?><?=""?>
+<#?=app()->enum("<?=$enum_set->getFullName()?>")->offsetGet(<?=$var_name?>["<?=$name?>"])?><#?="\n"?><?=""?>
 <?php else: /* elseif type=="enum_set" */ ?>
-<#?=<?=$var_name?>["<?=$name?>"]?><?=""?>
+<#?=<?=$var_name?>["<?=$name?>"]?><#?="\n"?><?=""?>
 <?php endif; /* if type=="assoc" */ ?>
