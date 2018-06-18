2c9154a8f2870beea4de5b4b9300b508e8cef55e
System <system@ap1>
2018-06-18 13:31:12 +0900

    Bugfix

--- lib/Debug/JavascriptRenderer.php
+++ lib/Debug/JavascriptRenderer.php
@@ -11,13 +11,13 @@ class JavascriptRenderer extends IlluminateJavascriptRenderer
             return parent::renderHead();
         }
 
-        $cssRoute = $this->url->route('debugbar.assets.css', [
+        $cssRoute = $this->url->route('debugbar.assets.css', array(
             'v' => $this->getModifiedTime('css')
-        ]);
+        ));
 
-        $jsRoute  = $this->url->route('debugbar.assets.js', [
+        $jsRoute  = $this->url->route('debugbar.assets.js', array(
             'v' => $this->getModifiedTime('js')
-        ]);
+        ));
 
         $html  = '';
         $html .= "<link rel='stylesheet' type='text/css' href='{$cssRoute}'>";
